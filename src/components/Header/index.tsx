7c7a1b39b5a6082c1290c883de77d97d0635ddda
IMvErickz <erickspy2003@gmail.com>
2024-07-04 17:02:43 -0300

    feat: add logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,8 @@
 export function Header() {
     return (
-        <header className="w-full flex items-center justify-center px-8">
-            <h1 className="text-slate-400 text-6xl font-mono leading-relaxed">FlowBite Test Components</h1>
+        <header className="w-full flex flex-col items-center justify-center px-8">
+            <img src="https://flowbite.com/docs/images/logo.svg" alt="" />
+            <h1 className="text-slate-400 text-6xl font-mono leading-relaxed">FlowBite Components</h1>
         </header>
     )
 }
